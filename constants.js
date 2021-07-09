031f0fcaf15142b15c53d7a163ab4d4cd68dfa28
pedrodev777 <tobiasspedro594@gmail.com>
2021-07-09 20:18:42 -0300

    commit

---
+++ constants.js
@@ -0,0 +1,12 @@
+module.exports.payload = {
+  "op": 2,
+  "d": {
+    "token": null,
+    "intents": 513,
+    "properties": {
+      "$os": "areo.js",
+      "$browser": "areo.js",
+      "$device": "areo.js"
+    }
+  }
+};
