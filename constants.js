b231864b399320a5f4770f3925543dd6bc08826b
pedrodev777 <tobiasspedro594@gmail.com>
2021-07-10 13:43:23 -0300

    commit

--- constants.js
+++ constants.js
@@ -10,3 +10,22 @@ module.exports.payload = {
     }
   }
 };
+
+module.exports.MessageTypes = {
+  0: 'default',
+  1: 'recipient_add',
+  2: 'recipient_remove',
+  3: 'call',
+  4: 'channel_name_change',
+  5: 'channel_icon_change',
+  6: 'pins_add',
+  7: 'guild_member_join',
+  8: 'user_premium_guild_subscription',
+  9: 'user_premium_guild_subscription_tier_1',
+  10: 'user_premium_guild_subscription_tier_2',
+  11: 'user_premium_guild_subscription_tier_3',
+  12: 'channel_follow_add',
+  14: 'guild_discovery_disqualified',
+  15: 'guild_discovery_requalified'
+
+}
